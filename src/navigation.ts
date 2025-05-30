061739ecc122da70ee2eccf860cf391dd10b24fd
aydocs <140649799+aydocs@users.noreply.github.com>
2025-05-30 16:34:46 +0300

    İlk commit

---
+++ src/navigation.ts
@@ -0,0 +1,25 @@
+import { getPermalink } from './utils/permalinks';
+
+export const headerData = {
+  links: [
+    { text: 'Ana Sayfa', href: getPermalink('/') },
+    { text: 'Blog', href: '/blog' },
+  ],
+  actions: [{ text: 'İletişim', href: getPermalink('/contact') }],
+};
+
+export const footerData = {
+  links: [
+    {
+      title: 'Menü',
+      links: [
+        { text: 'Ana Sayfa', href: getPermalink('/') },
+        { text: 'Blog', href: getPermalink('/blog') },
+
+      ],
+    },
+  ],
+  footNote: `
+    Made by <a class="text-blue-600 underline dark:text-muted" href="https://comcoore.com/">CCR Yazılım</a> · Tüm hakları saklıdır.
+  `,
+};
